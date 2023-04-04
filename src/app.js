991aa1cbb69ec6e5ac0f7086a865e7633e9baf85
umniknik <kaaaaaljan@mail.ru>
2023-04-04 14:18:23 +0300

    ДЗ «Рабочее окружение» 3 Задание ESLint - готово

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const characters = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'маг', health: 0},
-    {name: 'лучник', health: 0},
-  ];
-  
-  const alive = characters.filter(item => item.health > 0);
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'маг', health: 0 },
+  { name: 'лучник', health: 0 },
+];
+
+const alive = characters.filter((item) => item.health > 0);
+console.log('Участники у которых здоровье больше нуля: ', alive); // устранил " 8:7 error 'alive' is assigned a value but never used no-unused-vars"
